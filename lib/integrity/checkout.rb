d17676af61eb8b1c26265248c7d56a6c3a06a282
Grzegorz Derebecki <grzegorz.derebecki@fdb.pl>
2012-06-15 14:17:45 +0200

    make reset for checking out remote branch

--- lib/integrity/checkout.rb
+++ lib/integrity/checkout.rb
@@ -13,9 +13,7 @@ module Integrity
       in_dir do |c|
         c.run! "git fetch origin"
         c.run! "git checkout #{@repo.branch}"
-        c.run! "git reset --hard HEAD"
-        c.run! "git pull"
-        c.run! "git checkout #{@repo.branch}"
+        c.run! "git reset --hard origin/#{@repo.branch}"
       end
 
     end
