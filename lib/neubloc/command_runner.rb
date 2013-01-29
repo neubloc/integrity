cf8dc66c221c3e03e7ecbf32b0496b535dadefb2
Grzegorz Derebecki <grzegorz.derebecki@fdb.pl>
2013-01-29 09:37:13 +0100

    moving config to example and ignoring init.rb in git

--- lib/neubloc/command_runner.rb
+++ lib/neubloc/command_runner.rb
@@ -1,17 +1,17 @@
 module Neubloc
   class CommandRunner
-  
+
     class << self
       def run(command, options = {}, &block)
         new(command, options).run(&block)
       end
     end
-  
+
     attr_accessor :command
     attr_accessor :interval
     attr_accessor :output
     attr_accessor :timer
-  
+
     def initialize(command, options = {})
       assign_defaults
 
@@ -19,37 +19,37 @@ module Neubloc
         send("#{name}=", value)
       end
       self.command = command
-    
+
     end
-  
+
     def assign_defaults
-      self.interval = 0.1
+      self.interval = 1
     end
-  
+
     def reset
       self.output = ""
       self.timer  = Time.at(0)
     end
-  
+
     def add_output(data)
       self.output << data
     end
-  
+
     def get_output_and_reset
-      self.output.tap do 
+      self.output.tap do
         self.output = ""
         self.timer  = Time.now
       end
     end
-    
+
     def output?
       output != ""
     end
-  
+
     def yield?
       Time.now - self.timer > self.interval
     end
-  
+
     def run(&block)
       IO.popen(command, "r") do |io|
         reset
@@ -60,6 +60,6 @@ module Neubloc
         yield get_output_and_reset if output?
       end
     end
-  
+
   end
 end
--- public/application.js
+++ public/application.js
@@ -1,21 +1,21 @@
 var Build = {
-  
+
   initialize: function() {
     console.log("initializing build...")
     this.refresh();
   },
-  refresh: function() {    
+  refresh: function() {
     var building = $(".building");
     var output = $(".output");
     if (building.length > 0 && output.length > 0) {
       $('#content').load(window.location.pathname + " #build_container", function() {
-		    // scroll bottom
+        // scroll bottom
         $(".output").scrollTop($(".output")[0].scrollHeight);
 
-        setTimeout('Build.refresh()', 200);
+        setTimeout('Build.refresh()', 2000);
       });
     }
-  }  
+  }
 }
 
 $(function() {
